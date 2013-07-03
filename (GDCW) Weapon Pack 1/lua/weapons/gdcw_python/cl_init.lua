c09d814a431fc37d747b2b724fe566dc5bc3e4f1
[USAF] DarkFox <haydengrigsby@gmail.com>
2013-07-03 12:53:37 -0500

    Revert "uploaded current server files"
    This reverts commit 201746d44c1f2012457face153c7820427fff540.

--- (GDCW) Weapon Pack 1/lua/weapons/gdcw_python/cl_init.lua
+++
@@ -1,8 +0,0 @@
-include('shared.lua')
-
-SWEP.PrintName				= "Colt Python"			// 'Nice' Weapon name (Shown on HUD)	
-SWEP.Slot				= 3				// Slot in the weapon selection menu
-SWEP.SlotPos				= 1				// Position in the slot
-SWEP.DrawAmmo				= true				// Should draw the default HL2 ammo counter				// Should draw the default crosshair
-SWEP.DrawWeaponInfoBox			= true				// Should draw the weapon info box
-SWEP.BounceWeaponIcon   		= true				// Should the weapon icon bounce?
